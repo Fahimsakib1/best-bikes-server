6f44663f4903329bc82556c09d81396812aa6657
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2022-11-24 19:36:48 +0600

    added the user information to database

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run(){
     try{
         const categoriesCollection = client.db('bestBikes').collection('categories');
         const bikeDetailsCollection = client.db('bestBikes').collection('bikeDetailsBrandWise');
+        const usersCollection = client.db('bestBikes').collection('users');
 
         //get all the categories
         app.get('/categories', async(req, res) => {
@@ -38,8 +39,29 @@ async function run(){
         //get all the bike details based on category name
         app.get('/category/:id', async(req, res) => {
             const id = req.params.id;
-            const query = {category_id : id};
+            const query = {category_id :id};
             const result = await bikeDetailsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        //post user info to database when suer signs up and prevent user with same email try to multiple sign in.
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            console.log(user);
+
+            const query = {
+                email: user.email 
+            }
+
+            const findAlreadyUserInDataBase = await usersCollection.find(query).toArray();
+            console.log("User already in database", findAlreadyUserInDataBase.length);
+
+            if (findAlreadyUserInDataBase.length) {
+                const message = 'This Email Already Exists';
+                return res.send({ acknowledged: false, message });
+            }
+
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         })
     }
