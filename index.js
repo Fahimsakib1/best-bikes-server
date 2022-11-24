10f08807bb7431ccabd1e00aab4135ce0200746a
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2022-11-24 07:00:05 +0600

    get the categories data and show the  bike details based on id

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -12,6 +13,45 @@ app.use(express.json());
 require('dotenv').config();
 
 
+//userrName: bestBikes
+//password: DOkVGAHDhzxeVVXA
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.axoxgat.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run(){
+    try{
+        const categoriesCollection = client.db('bestBikes').collection('categories');
+        const bikeDetailsCollection = client.db('bestBikes').collection('bikeDetailsBrandWise');
+
+        //get all the categories
+        app.get('/categories', async(req, res) => {
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        //get all the bike details based on category name
+        app.get('/category/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {category_id : id};
+            const result = await bikeDetailsCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+
+    finally{
+
+    }
+}
+run().catch(error => console.log(error))
+
+
+
 app.get('/', (req, res) => {
     res.send('Best Bikes Server is running');
 })
@@ -19,3 +59,5 @@ app.get('/', (req, res) => {
 app.listen(port, (req, res) => {
     console.log('Best Bikes Server is running on Port', port)
 })
+
+
